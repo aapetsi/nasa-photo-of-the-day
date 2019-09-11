91e40e21aa445a4b4a1d9a56c4ca192d896d9ab1
aapetsi <apetsiampiah@gmail.com>
2019-09-11 14:09:35 +0000

    Setup app component to fetch data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
+import POD from "./components/POD";
+
+const App = () => {
+  const [, setPictureOfTheDay] = useState("");
+
+  useEffect(() => {
+    fetch("https://lambda-github-api-server.herokuapp.com/")
+      .then(res => res.json())
+      .then(data => {
+        let imageURL = data.url;
+        setPictureOfTheDay(imageURL);
+        console.log(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
+      <POD />
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun !
       </p>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/POD.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const POD = () => {
+    return (
+        <div>
+            <h1>this is from the pod component</h1>
+        </div>
+    )
+}
+
+export default POD
