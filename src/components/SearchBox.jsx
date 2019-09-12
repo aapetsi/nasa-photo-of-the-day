1fe685c73411671b2c19081506a3c93268dd074e
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-09-12 13:13:28 +0000

    Add styling for h1 in SearchBox

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
+let defaultDate = new Date();
+
 const WrapperDiv = styled.div`
   margin: 0 auto;
   font-size: 24px;
 `;
 
+const Typography = styled.h1`
+  font-size: 28px;
+  text-align: center;
+`;
+
 const SearchBox = ({ handleFormSubmit }) => {
   return (
     <WrapperDiv>
+      <Typography>Or search for image by date</Typography>
       <form onSubmit={handleFormSubmit}>
         <input name="date" type="date" />
         <button type="submit">Search</button>
