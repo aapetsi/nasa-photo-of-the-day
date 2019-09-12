a0558e9ad7234cd67a5642f4c0169ffa390dc5b7
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-09-12 12:52:40 +0000

    Add search box to query by date

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import POD from "./components/POD";
+import SearchBox from "./components/SearchBox";
 
 const WrapperDiv = styled.div`
   margin: 10px auto;
@@ -9,13 +10,15 @@ const WrapperDiv = styled.div`
   font-family: "Chilanka", cursive;
   border: 2px solid purple;
   border-radius: 30px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const App = () => {
   const [pictureOfTheDay, setPictureOfTheDay] = useState({});
   // https://lambda-github-api-server.herokuapp.com/
   useEffect(() => {
-    fetch("https://lambda-github-api-server.herokuapp.com/")
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
       .then(res => res.json())
       .then(data => {
         let imageURL = data;
@@ -25,8 +28,24 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
 
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    let queryDate = e.target.date.value;
+    // get picture from nasa api
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${queryDate}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        let imageURL = data;
+        setPictureOfTheDay(imageURL);
+      });
+    console.log(e.target.date.value);
+  };
+
   return (
     <WrapperDiv>
+      <SearchBox handleFormSubmit={handleFormSubmit} />
       <POD data={pictureOfTheDay} />
     </WrapperDiv>
   );
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
 
-const SearchBox = () => {
-    <div>
-        <input type="search"/>
-    </div>
-}
+const WrapperDiv = styled.div`
+  margin: 0 auto;
+  font-size: 24px;
+`;
+
+const SearchBox = ({ handleFormSubmit }) => {
+  return (
+    <WrapperDiv>
+      <form onSubmit={handleFormSubmit}>
+        <input name="date" type="date" />
+        <button type="submit">Search</button>
+      </form>
+    </WrapperDiv>
+  );
+};
 
 export default SearchBox;
